78bd683f96779c3caf95eeb50ede09688de3e0de
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-04-21 15:36:12 +0300

    stage changes

--- input/input_demo.py
+++ input/input_demo.py
@@ -1,70 +1,42 @@
-# numbers = [1, 2, 3, 4, 5]
-# for i in numbers:
-#     if i % 2 == 0:
-#        print(i)
 
-# number = 0
-# while True:
-#     print("I love candy "+ str(number))
-#     number +=1
-#     if number == 7 :
-#         break 
+# def personal_details():
+#     name, age = "Agnes", 19
+#     email = "aggieletty24"
+#     favouriteMovie = "Bridgerton"
+#     print("Name: {}\nAge: {}\nEmail: {}\nFavouriteMovie: {}".format(name, age,email,favouriteMovie))
 
-def personal_details():
-    name, age = "Flora", 19
-    email = "florachirry80"
-    favouriteMovie = "Bridgerton"
-    print("Name: {}\nAge: {}\nEmail: {}\nFavouriteMovie: {}".format(name, age,email,favouriteMovie))
+# personal_details()
 
-personal_details()
+def main():
+    #Empty dictionary to collect users
+    responses={}
+    
+    #set flag condition to end or continue loop
+    polling=True
 
-# # taken numbers
-# numTaken = [3,5,7,11,13]
+    #while loop to fill responses
+    def main():
+    #Empty dictionary to collect users
+         responses={}
+    
+    #set flag condition to end or continue loop
+    polling=True
 
-# print("Available numbers")
-
-# # loop
-# for i in range(1,21):
-#     if i in numTaken:
-#         continue
-#     print(i)
-
-# list_a = ["a","b","c","d"] 
-# list_b = [1,2,3,4,5,6]
-
-# Joining list_b to list_a
-
-# list_a.extend(list_b)
-
-# print(list_a) # this will print ["a","b","c","d",1,2,3,4,5,6]
-# list_a = ["a","b","c","d"] 
-# list_b = [1,2,3,4,5,6]
-
-# # Joining list_b to list_a
-
-# list_a.extend(list_b)
-
-# print(list_a) # this will print ["a","b","c","d",1,2,3,4,5,6]
-# print(list_b) # this will print [1,2,3,4,5,6]
-
-# name = "James"
-# age = 19
-
-# print(f"My name is {name} and I am {age} years old")
-
-# print out u users name and age
-# name = "James"
-# age = 19
-
-# print("My name is " + name + " I am " + str(age) + " years old")
+    #while loop to fill responses
+    while polling:
+        email=input("Enter email: ")
+        name=input("Agnes")
+        age=input("18")
+        favouriteMovie=input("Merlin")
+        responses[name]=email
+        responses[name]=name
+        responses[name]=age
+        responses[name]=  favouriteMovie
+        rep=input("Any more users y/n?")
+        if rep=='n' or rep=='N':
+            polling=False
+    for info in responses.items():
+        print("\nEmail: \t\t"+email+"\nName: \t\t"+name+"\nAge: \t\t"+age+ "\nFavouriteMovie: \t\t"+favouriteMovie )
+main()
 
 
-
-# greetings = 'Hello, Moringa!'
-
-# part_two = greetings[7:15]
-# # print(part_two)
-
-# def fun_a(a,b):
-#   print(a+b)
-# fun_a(1,4)
