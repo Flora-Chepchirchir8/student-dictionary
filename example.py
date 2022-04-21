efe76086c12e511e953dd49fe06d56e8909bfea4
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-04-21 14:34:31 +0300

    initial commit

---
+++ example.py
@@ -0,0 +1,3 @@
+from math import pi
+r = float(input ("random_number = random.randint(0,10)"))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
---
+++ input/input_demo.py
@@ -0,0 +1,70 @@
+# numbers = [1, 2, 3, 4, 5]
+# for i in numbers:
+#     if i % 2 == 0:
+#        print(i)
+
+# number = 0
+# while True:
+#     print("I love candy "+ str(number))
+#     number +=1
+#     if number == 7 :
+#         break 
+
+def personal_details():
+    name, age = "Flora", 19
+    email = "florachirry80"
+    favouriteMovie = "Bridgerton"
+    print("Name: {}\nAge: {}\nEmail: {}\nFavouriteMovie: {}".format(name, age,email,favouriteMovie))
+
+personal_details()
+
+# # taken numbers
+# numTaken = [3,5,7,11,13]
+
+# print("Available numbers")
+
+# # loop
+# for i in range(1,21):
+#     if i in numTaken:
+#         continue
+#     print(i)
+
+# list_a = ["a","b","c","d"] 
+# list_b = [1,2,3,4,5,6]
+
+# Joining list_b to list_a
+
+# list_a.extend(list_b)
+
+# print(list_a) # this will print ["a","b","c","d",1,2,3,4,5,6]
+# list_a = ["a","b","c","d"] 
+# list_b = [1,2,3,4,5,6]
+
+# # Joining list_b to list_a
+
+# list_a.extend(list_b)
+
+# print(list_a) # this will print ["a","b","c","d",1,2,3,4,5,6]
+# print(list_b) # this will print [1,2,3,4,5,6]
+
+# name = "James"
+# age = 19
+
+# print(f"My name is {name} and I am {age} years old")
+
+# print out u users name and age
+# name = "James"
+# age = 19
+
+# print("My name is " + name + " I am " + str(age) + " years old")
+
+
+
+# greetings = 'Hello, Moringa!'
+
+# part_two = greetings[7:15]
+# # print(part_two)
+
+# def fun_a(a,b):
+#   print(a+b)
+# fun_a(1,4)
